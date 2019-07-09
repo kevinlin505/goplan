3dffae934516bfb51fced31ec2e308deb101fec9
kevinlin505 <kevinlin505@hotmail.com>
2019-07-08 22:03:49 -0400

    Basic create trip connetion

---
+++ src/providers/trip/trip.js
@@ -0,0 +1,56 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import trip from '@data/trip';
+
+export const types = {
+  CREATE_TRIP: 'TRIP/CREATE_TRIP',
+};
+
+const initialState = {};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case types.CREATE_TRIP: {
+      return {
+        ...state,
+      };
+    }
+
+    default:
+      return state;
+  }
+}
+
+export const tripActions = {
+  createTrip: formDetails => dispatch => {
+    const tripDetails = {
+      ...formDetails,
+      destinations: [
+        {
+          country: 'Peru',
+          geo: new firebase.firestore.GeoPoint(-72.5471516, -13.1631412),
+          name: 'Machu Picchu',
+        },
+      ],
+      end_date: new Date(2019, 8, 20).getTime(),
+      estimate_budget_per_person: 2000,
+      expenses: [],
+      name: 'Trip to Peru',
+      notes: 'Backpacking trip to Machu Picchu.',
+      spending: 0,
+      start_date: new Date(2019, 7, 31).getTime(),
+    };
+
+    trip()
+      .createTrip(tripDetails)
+      .then(() => {
+        console.log('trip created');
+        dispatch({
+          type: types.CREATE_TRIP,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+};
