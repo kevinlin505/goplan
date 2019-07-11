fbb0a79bfca3821233b24039b47c6f76025cb6dd
Leo Feng <wleofeng@gmail.com>
2019-07-11 19:34:36 -0400

    changed function params

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,8 @@
-/**
- * Copyright 2015 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 'use strict';
 
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
-// Configure the email transport using the default SMTP transport and a GMail account.
-// For Gmail, enable these:
-// 1. https://www.google.com/settings/security/lesssecureapps
-// 2. https://accounts.google.com/DisplayUnlockCaptcha
-// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
-// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+
 const gmailEmail = functions.config().gmail.email;
 const gmailPassword = functions.config().gmail.password;
 const mailTransport = nodemailer.createTransport({
@@ -33,30 +13,26 @@ const mailTransport = nodemailer.createTransport({
   },
 });
 
-// Your company name to include in the emails
-// TODO: Change this to your app or company name to customize the email sent.
-const APP_NAME = 'Cloud Storage for Firebase quickstart';
+const APP_NAME = 'GoPlan';
 
-// Saves a message to the Firebase Realtime Database but sanitizes the text by removing swearwords.
 exports.sendInvitationEmail = functions.https.onCall((data, context) => {
-  const email = data.email; // The email of the user.
-  const displayName = data.displayName; // The display name of the user.
-  // [END eventAttributes]
+  const email = data.email; 
+  const invitationLink = data.invitationLink;
 
-  return sendInvitationEmail(email, displayName);
+  return sendInvitationEmail(email, invitationLink);
 });
 
 // Sends a welcome email to the given user.
-async function sendInvitationEmail(email, displayName) {
+async function sendInvitationEmail(email, invitationLink) {
   const mailOptions = {
     from: `${APP_NAME} <noreply@firebase.com>`,
     to: email,
   };
 
   // The user subscribed to the newsletter.
-  mailOptions.subject = `Welcome to ${APP_NAME}!`;
-  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. Please join GoPlan.`;
+  mailOptions.subject = `Invitation to join ${APP_NAME}!`;
+  mailOptions.text = `Please join GoPlan. Link: ${invitationLink}`;
   await mailTransport.sendMail(mailOptions);
-  console.log('New welcome email sent to:', email);
+  console.log('Invitation email sent to:', email);
   return null;
 }
