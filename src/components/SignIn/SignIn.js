9f08869c00b28703cf8a197ca943cf07edb1e3a5
kevinlin505 <kevinlin505@hotmail.com>
2019-07-11 21:22:05 -0400

    clean up some css on login

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -6,6 +6,7 @@ import { bindActionCreators } from 'redux';
 import { authActions } from '@providers/auth/auth';
 import GoogleAuthButton from '@components/SignIn/AuthButton/GoogleAuthButton';
 import FacebookAuthButton from '@components/SignIn/AuthButton/FacebookAuthButton';
+import backgroundImg from '@assets/images/background.jpg';
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -43,38 +44,31 @@ SignIn.propTypes = {
 };
 
 const Background = styled.div`
-  background-image: url('https://images.unsplash.com/photo-1462216150495-f0bac6474243?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80');
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center center;
-  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 0;
+  min-height: 100vh;
+  background-image: url(${backgroundImg});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
 `;
 
 const ContentContainer = styled.div`
-  margin: 0 auto;
-  padding: 20px;
-  transform: translateY(5px);
-  width: 400px;
-  background-color: white;
-  box-shadow: rgba(198, 208, 235, 0.5) 0px 20px 40px;
   position: relative;
-  text-align: center;
+  padding: 40px;
+  width: 400px;
+  background: rgba(255, 255, 255, 0.75);
+  box-shadow: rgba(198, 208, 235, 0.5) 0px 20px 40px;
   box-sizing: border-box;
-  max-height: 95%;
-  animation: 0.3s cubic-bezier(0.2, 0.8, 0.2, 1) 0.2s 1 normal forwards running
-    iHNdoB;
+  text-align: center;
   overflow: auto;
-  border-radius: 20px;
-  transition: all 0.8s cubic-bezier(0.2, 0.8, 0.2, 1.2) 0s;
 `;
 
 const LoginButtonContainer = styled.div`
-  margin: 0 auto;
+  margin: 5px auto;
 `;
+
 const Header = styled.div`
   margin-bottom: 20px;
   font-size: 20px;
