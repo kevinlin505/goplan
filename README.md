21c2c587045c3873812c21f1c142b0969060aa9a
Leo Feng <wleofeng@gmail.com>
2019-07-11 20:36:13 -0400

    fixes

--- README.md
+++ README.md
@@ -5,3 +5,20 @@
 - first time run `$ yarn && yarn start` or `$ npm && npm start` in commnad line. Otherwise just run `yarn start` or `npm start` to start the app.
 - The app is running on a `webpack-dev-server` on `localhost:3000`
 - Currently, the `Keys.js` under the `constants/` directory is missing, you will have to create a `Keys.js` file and paste the `Keys` object containing the `FIREBASE` object in order to connect to the firebase.
+
+
+#### Functions:
+
+1. You must have the Firebase CLI installed. If you don't have it install it with npm install -g firebase-tools and then configure it with firebase login.
+
+2. Configure the CLI locally by using firebase use --add and select your project in the list.
+
+3. Install Cloud Functions dependencies locally by running: cd functions; npm install; cd -
+
+4. To be able to send emails with your Gmail account: enable access to Less Secure Apps and Display Unlock Captcha. For accounts with 2-step verification enabled Generate an App Password.
+
+5. Set the gmail.email and gmail.password Google Cloud environment variables to match the email and password of the Gmail account used to send emails (or the app password if your account has 2-step verification enabled). For this use:
+
+firebase functions:config:set gmail.email="myusername@gmail.com" gmail.password="secretpassword"
+
+6. run this to deploy: firebase deploy --only functions:sendInvitationEmail
---
+++ functions/.eslintrc.js
@@ -0,0 +1,102 @@
+const rules = {
+  'arrow-body-style': 'off',
+  'arrow-parens': 'off',
+  'class-methods-use-this': 'off',
+  'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  'import/no-named-as-default': 'off', // issue with default export of connected components
+  'import/no-unresolved': 'error',
+  'no-mixed-operators': 'off',
+  'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+  'no-return-assign': 'warn',
+  'no-var': 'off',
+  'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
+  'prefer-destructuring': 'warn',
+  radix: ['error', 'as-needed'],
+
+  // React-specific
+  'react/destructuring-assignment': 'warn',
+  'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  'react/jsx-sort-props': [
+    1,
+    {
+      reservedFirst: true,
+    },
+  ],
+  'react/forbid-prop-types': 'off',
+  'react/no-array-index-key': 'off',
+  'react/no-did-mount-set-state': 'off',
+  'react/no-will-update-set-state': 'warn',
+  'react/no-did-update-set-state': 'off',
+  'react/sort-comp': [
+    2,
+    {
+      order: [
+        'static-methods',
+        'instance-variables',
+        'lifecycle',
+        'everything-else',
+        'render',
+      ],
+    },
+  ],
+};
+
+module.exports = {
+  /*
+    "plugin:prettier/recommended"
+      - enables eslint-plugin-prettier
+      - extends eslint-config-prettier
+      - sets rule "prettier/prettier": "error" - tells ESlint to run prettier on `--fix`
+  */
+  extends: [
+    'airbnb-base',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+  ],
+
+  parser: 'babel-eslint',
+
+  plugins: ['import', 'react-hooks'],
+
+  root: true,
+
+  globals: {
+    window: false,
+    document: false,
+    fetch: false,
+  },
+
+  env: {
+    browser: true,
+  },
+
+  overrides: [
+    {
+      files: ['**/webpack*'],
+      rules: {
+        ...rules,
+        'global-require': 'off',
+        'import/no-dynamic-require': 'off',
+        'import/prefer-default-export': 'off',
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+
+  rules: rules,
+
+  settings: {
+    'import/resolver': {
+      alias: [
+        ['@components', './src/components'],
+        ['@constants', './src/constants'],
+        ['@data', './src/data'],
+        ['@providers', './src/providers'],
+        ['@store', './src/store'],
+        ['@styles', './src/styles'],
+        ['@utils', './src/utils'],
+      ],
+    },
+  },
+};
