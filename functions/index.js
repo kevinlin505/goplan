cbb4bed3142f8921b6aa9b9df2a0f25ea7496d96
Leo Feng <wleofeng@gmail.com>
2019-07-21 17:45:55 -0400

    added unsplash func

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
+const axios = require('axios');
 
+// Invitation Email
 const gmailEmail = functions.config().gmail.email;
 const gmailPassword = functions.config().gmail.password;
 const mailTransport = nodemailer.createTransport({
@@ -28,9 +30,40 @@ exports.sendInvitationEmail = functions.https.onCall(data => {
   return sendInvitationEmail(data.email, data.invitationLink);
 });
 
-exports.getAPIKeys = functions.https.onCall(() => {
-  return {
-    unsplashAccessToken:
-      '7110f5de36f381d4394475668a2e656f8100a914a8e73d5b4e6e2d469e15296d',
-  };
+// Unsplash
+const unsplashHelper = {
+  apiUrl: (accessToken, query) => {
+    return `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${accessToken}&orientation=landscape`;
+  },
+  imageUrl: (sourceUrl, sizeParam = '&w=600&h=300') => {
+    return `${sourceUrl}${sizeParam}`;
+  },
+};
+
+async function getImage(url) {
+  return axios
+    .get(url)
+    .then(response => {
+      const results = response.data.results;
+      if (results.length > 0) {
+        const imageSourceUrl = results[0].urls.full;
+        const resizedUrl = unsplashHelper.imageUrl(imageSourceUrl);
+        console.log(`data: ${resizedUrl}`);
+        return resizedUrl;
+      }
+      return null;
+    })
+    .catch(err => {
+      console.error(`erorr: ${err}`);
+      return null;
+    });
+}
+
+exports.getUnsplashImage = functions.https.onCall(query => {
+  const url = unsplashHelper.apiUrl(
+    '7110f5de36f381d4394475668a2e656f8100a914a8e73d5b4e6e2d469e15296d',
+    query,
+  );
+  console.log(`url: ${url}`);
+  return getImage(url);
 });
