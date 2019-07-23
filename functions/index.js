1df6e3d53228898f2e20a99a3ecacb16a4c327d7
Leo Feng <wleofeng@gmail.com>
2019-07-22 21:54:27 -0400

    Lf image (#22)
    added getUnsplashImage function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
-'use strict';
-
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
+const axios = require('axios');
+const keys = require('./constants/Keys');
 
+// Invitation Email
 const gmailEmail = functions.config().gmail.email;
 const gmailPassword = functions.config().gmail.password;
 const mailTransport = nodemailer.createTransport({
@@ -15,24 +16,52 @@ const mailTransport = nodemailer.createTransport({
 
 const APP_NAME = 'GoPlan';
 
-exports.sendInvitationEmail = functions.https.onCall((data, context) => {
-  const email = data.email; 
-  const invitationLink = data.invitationLink;
-
-  return sendInvitationEmail(email, invitationLink);
-});
-
-// Sends a welcome email to the given user.
 async function sendInvitationEmail(email, invitationLink) {
   const mailOptions = {
     from: `${APP_NAME} <noreply@firebase.com>`,
     to: email,
   };
-
-  // The user subscribed to the newsletter.
   mailOptions.subject = `Invitation to join ${APP_NAME}!`;
   mailOptions.text = `Please join GoPlan. Link: ${invitationLink}`;
   await mailTransport.sendMail(mailOptions);
-  console.log('Invitation email sent to:', email);
   return null;
 }
+
+exports.sendInvitationEmail = functions.https.onCall(data => {
+  return sendInvitationEmail(data.email, data.invitationLink);
+});
+
+// Unsplash
+const unsplashHelper = {
+  apiUrl: (accessToken, query) => {
+    return `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${accessToken}&orientation=landscape`;
+  },
+  imageUrl: (sourceUrl, sizeParam = '&w=600&h=300') => {
+    return `${sourceUrl}${sizeParam}`;
+  },
+};
+
+async function getImage(url) {
+  return axios
+    .get(url)
+    .then(response => {
+      const results = response.data.results;
+      if (results.length > 0) {
+        const imageSourceUrl = results[0].urls.full;
+        const resizedUrl = unsplashHelper.imageUrl(imageSourceUrl);
+        console.log(`data: ${resizedUrl}`);
+        return resizedUrl;
+      }
+      return null;
+    })
+    .catch(err => {
+      console.error(`erorr: ${err}`);
+      return null;
+    });
+}
+
+exports.getUnsplashImage = functions.https.onCall(query => {
+  const url = unsplashHelper.apiUrl(keys.UNSPLASH.accessToken, query);
+  console.log(`url: ${url}`);
+  return getImage(url);
+});
