85075ebff82f37a10de4f7e44b08d0b3704f3f75
Leo Feng <wleofeng@gmail.com>
2019-08-04 21:52:53 -0400

    reorder func

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
+const getUnsplashImage = require('./modules/GetUnsplashImage');
 const sendInvitationEmail = require('./modules/SendInvitationEmail');
 const uploadReceipt = require('./modules/UploadReceipt');
-const getUnsplashImage = require('./modules/GetUnsplashImage');
 
+exports.getUnsplashImage = getUnsplashImage;
 exports.sendInvitationEmail = sendInvitationEmail;
 exports.uploadReceipt = uploadReceipt;
-exports.getUnsplashImage = getUnsplashImage;
