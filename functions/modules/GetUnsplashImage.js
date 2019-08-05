97f88834bc7fcc55b023d24a999f3de33b153265
Leo Feng <wleofeng@gmail.com>
2019-08-04 23:53:48 -0400

    refactor

--- functions/modules/GetUnsplashImage.js
+++ functions/modules/GetUnsplashImage.js
@@ -2,7 +2,6 @@ const functions = require('firebase-functions');
 const axios = require('axios');
 const keys = require('../constants/Keys');
 
-// Unsplash
 const unsplashHelper = {
   apiUrl: (accessToken, query) => {
     return `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${accessToken}&orientation=landscape`;
--- functions/modules/UploadReceipt.js
+++ functions/modules/UploadReceipt.js
@@ -4,22 +4,27 @@ const AWS = require('aws-sdk/global');
 const S3 = require('aws-sdk/clients/s3');
 const keys = require('../constants/Keys');
 
-const getBase64Helper = dataString => {
-    const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-    var response = {};
-  
-    if (matches.length !== 3) {
-      return new Error('Invalid input string');
+const UploadHelper = {
+    imageUrl: (bucketName, tripId, uniqueKey) => {
+        return `https://${bucketName}.s3.amazonaws.com/expense/${tripId}/${uniqueKey}`    
+    },
+    getBase64: dataString => {
+        const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+        var response = {};
+      
+        if (matches.length !== 3) {
+          return new Error('Invalid input string');
+        }
+      
+        response.type = matches[1];
+        response.data = new Buffer(matches[2], 'base64');
+      
+        return response;
     }
-  
-    response.type = matches[1];
-    response.data = new Buffer(matches[2], 'base64');
-  
-    return response;
-};
-  
+}
+
 const uploadToS3 = async options => {
-    const buff = getBase64Helper(options.data).data;
+    const buff = UploadHelper.getBase64(options.data).data;
     const params = {
         Bucket: `${keys.AWS.bucketName}/expense/${options.tripId}`,
         Key: uuidv3(options.name, keys.AWS.uuid),
@@ -45,9 +50,9 @@ const uploadToS3 = async options => {
     });
     return promise.then(res => {
         if (res) {
-        const url = `https://${keys.AWS.bucketName}.s3.amazonaws.com/expense/${options.tripId}/${params.Key}`;
-        console.log(url);
-        return url;
+            const url = UploadHelper.imageUrl(keys.AWS.bucketName, options.tripId, params.Key);
+            console.log(url);
+            return url;
         }
         return null;
     });
