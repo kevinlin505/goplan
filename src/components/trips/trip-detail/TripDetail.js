12ea4553a6d73636225ee40e01e879bc9e366de6
Yong Bin Gao <ben.gao25@gmail.com>
2019-08-07 19:50:16 -0400

    Add weather information for destinations (#44)
    * Add weather api
    
    * Add weather infomation to destinations
    
    * Remove unused code
    
    * Change weather icon to use openweather icons

--- src/components/trips/trip-detail/TripDetail.js
+++ src/components/trips/trip-detail/TripDetail.js
@@ -5,12 +5,14 @@ import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Button, Divider, withStyles } from '@material-ui/core';
+import openWeatherApi from '@utils/openWeatherApi';
 import { expenseActions } from '@providers/expense/expense';
 import { userActions } from '@providers/user/user';
 import { tripActions } from '@providers/trip/trip';
 import { getParamTripId, getTripStatus } from '@selectors/tripSelector';
 import googleMapsApi from '@utils/googleMapsApi';
 import validateEmail from '@utils/validateEmail';
+import getTravelDates from '@utils/calculateTravelDates';
 import TripMembers from '@components/trips/trip-detail/trip-members/TripMembers';
 import TripMap from '@components/trips/trip-detail/trip-map/TripMap';
 import TripExpenseSummary from '@components/trips/trip-detail/trip-expense/TripExpenseSummary';
@@ -51,6 +53,9 @@ const TripDetail = ({
 }) => {
   const [isExpenseModal, setExpenseModal] = useState(false);
   const [expenseList, setExpenseList] = useState(null);
+  const [weatherObject, setWeatherObject] = useState({});
+  const [inviteEmail, setInviteEmail] = useState('');
+  const [validEmail, setValidEmail] = useState(false);
   const google = googleMapsApi();
   const showTripCard =
     trip.selectedTrip && trip.selectedTrip.id === match.params.tripId;
@@ -62,33 +67,15 @@ const TripDetail = ({
     trip.selectedTrip &&
     new Date(trip.selectedTrip.travelDates.endAt).toLocaleDateString();
 
-  const toggleCreateExpenseModal = () => {
-    setExpenseModal(!isExpenseModal);
-  };
+  function toggleCreateExpenseModal() {
+    setExpenseModal(prevExpenseModal => !prevExpenseModal);
+  }
 
-  useEffect(() => {
-    actions.trip.getTrip(tripId).then(data => {
-      actions.user.getAllMembers(data.selectedTrip.members);
+  function handleLeaveTrip() {
+    actions.trip.leaveTrip(tripId).then(() => {
+      history.push('/home');
     });
-
-    if (!userInTrip) {
-      actions.trip.joinTrip(tripId);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (trip.selectedTrip) {
-      actions.trip.getTripExpenses(trip.selectedTrip.expenses);
-      setExpenseList(trip.selectedTrip.expenses.map(ele => ele.id));
-    }
-  }, [trip.selectedTrip]);
-
-  const [inviteEmail, setInviteEmail] = useState('');
-  const [validEmail, setValidEmail] = useState(false);
-
-  useEffect(() => {
-    setValidEmail(validateEmail(inviteEmail));
-  }, [inviteEmail]);
+  }
 
   function handleInviteEmail(event) {
     setInviteEmail(event.target.value);
@@ -106,10 +93,41 @@ const TripDetail = ({
     }
   }
 
-  function handleLeaveTrip() {
-    actions.trip.leaveTrip(tripId).then(() => {
-      history.push('/home');
+  function getWeatherInfo(destinations) {
+    const weather = { ...weatherObject };
+    destinations.forEach(destination => {
+      openWeatherApi(destination.geo).then(resp => {
+        weather[destination.placeId] = {
+          condition: resp.weather[0].main,
+          icon: `http://openweathermap.org/img/wn/${resp.weather[0].icon}@2x.png`,
+          temperature: resp.main.temp,
+        };
+
+        setWeatherObject(weather);
+      });
     });
+  }
+
+  function renderWeatherObject(placeId) {
+    const destinationWeather = weatherObject[placeId];
+
+    return destinationWeather ? (
+      <DestinationWeather>
+        <div>Current weather condition</div>
+        <WeatherInfo>
+          <WeatherIcon>
+            <WeatherIconImage
+              alt={destinationWeather.condition}
+              src={destinationWeather.icon}
+            ></WeatherIconImage>
+          </WeatherIcon>
+          <WeatherTemperature>
+            {destinationWeather.temperature.toFixed(1)} &#176;F
+          </WeatherTemperature>
+        </WeatherInfo>
+        <div>{destinationWeather.condition}</div>
+      </DestinationWeather>
+    ) : null;
   }
 
   function renderDestinations() {
@@ -125,15 +143,31 @@ const TripDetail = ({
               <DestinationPhoto
                 destinationPhoto={destination.photo.imageSourceUrl}
               ></DestinationPhoto>
-              <DestinationWeather>
-                <div>Weather Info</div>
-              </DestinationWeather>
+              {renderWeatherObject(destination.placeId)}
             </DestinationInfo>
           </div>
         </DestinationContainer>
       );
     });
   }
+
+  useEffect(() => {
+    setValidEmail(validateEmail(inviteEmail));
+  }, [inviteEmail]);
+
+  useEffect(() => {
+    actions.trip.getTrip(tripId).then(data => {
+      actions.user.getAllMembers(data.selectedTrip.members);
+      actions.trip.getTripExpenses(data.selectedTrip.expenses);
+      getWeatherInfo(data.selectedTrip.destinations);
+
+      setExpenseList(data.selectedTrip.expenses.map(ele => ele.id));
+    });
+
+    if (!userInTrip) {
+      actions.trip.joinTrip(tripId);
+    }
+  }, []);
 
   // Need to clean up this part, we should not get into this component if selectedTrip is null.
   return (
@@ -333,10 +367,33 @@ const DestinationPhoto = styled.div`
 
 const DestinationWeather = styled.div`
   padding: 10px;
+  font-size: 16px;
 `;
 
 const TripExpenseDetailsHeader = styled.div`
   font-size: 18px;
+`;
+
+const WeatherInfo = styled.div`
+  margin: 5px 0;
+  display: flex;
+  align-items: center;
+`;
+
+const WeatherIcon = styled.div`
+  width: 30px;
+  height: 30px;
+  overflow: hidden;
+  margin: 0 10px 0 5px;
+`;
+
+const WeatherIconImage = styled.img`
+  transform: scale(1.4);
+  width: 30px;
+`;
+
+const WeatherTemperature = styled.div`
+  font-size: 20px;
 `;
 
 export default connect(
