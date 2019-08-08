62d057197396dcc248a907188f1bb9f0bc3fd870
Leo Feng <wleofeng@gmail.com>
2019-08-07 21:45:49 -0400

    Weather function (#45)
    weather function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const getUnsplashImage = require('./modules/GetUnsplashImage');
+const getWeather = require('./modules/GetWeather');
 const sendInvitationEmail = require('./modules/SendInvitationEmail');
 const uploadReceipt = require('./modules/UploadReceipt');
 
 exports.getUnsplashImage = getUnsplashImage;
+exports.getWeather = getWeather;
 exports.sendInvitationEmail = sendInvitationEmail;
 exports.uploadReceipt = uploadReceipt;
--- src/components/trips/trip-detail/TripDetail.js
+++ src/components/trips/trip-detail/TripDetail.js
@@ -5,7 +5,6 @@ import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Button, Divider, withStyles } from '@material-ui/core';
-import openWeatherApi from '@utils/openWeatherApi';
 import { expenseActions } from '@providers/expense/expense';
 import { userActions } from '@providers/user/user';
 import { tripActions } from '@providers/trip/trip';
@@ -96,15 +95,17 @@ const TripDetail = ({
   function getWeatherInfo(destinations) {
     const weather = { ...weatherObject };
     destinations.forEach(destination => {
-      openWeatherApi(destination.geo).then(resp => {
-        weather[destination.placeId] = {
-          condition: resp.weather[0].main,
-          icon: `http://openweathermap.org/img/wn/${resp.weather[0].icon}@2x.png`,
-          temperature: resp.main.temp,
-        };
+      actions.trip
+        .getWeather(destination.geo.latitude, destination.geo.longitude)
+        .then(resp => {
+          weather[destination.placeId] = {
+            condition: resp.weather[0].main,
+            icon: `http://openweathermap.org/img/wn/${resp.weather[0].icon}@2x.png`,
+            temperature: resp.main.temp,
+          };
 
-        setWeatherObject(weather);
-      });
+          setWeatherObject(weather);
+        });
     });
   }
 
