d966814d2268b2a5c557810eb3f44ad87be42ece
Leo Feng <wleofeng@gmail.com>
2019-08-15 21:26:32 -0400

    WIP

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,13 @@
 const deleteReceipts = require('./modules/DeleteReceipts')
 const getUnsplashImage = require('./modules/GetUnsplashImage');
 const getWeather = require('./modules/GetWeather');
+const leaveTrip = require('./modules/LeaveTrip');
 const sendInvitationEmail = require('./modules/SendInvitationEmail');
 const uploadReceipt = require('./modules/UploadReceipt');
 
 exports.deleteReceipts = deleteReceipts;
 exports.getUnsplashImage = getUnsplashImage;
 exports.getWeather = getWeather;
+exports.leaveTrip = leaveTrip;
 exports.sendInvitationEmail = sendInvitationEmail;
 exports.uploadReceipt = uploadReceipt;
---
+++ functions/modules/LeaveTrip.js
@@ -0,0 +1,36 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+admin.initializeApp();
+const db = admin.firestore();
+
+const leaveTrip = options => {
+    const tripExpenses = options.tripExpenses;
+    const batch = db.batch();
+    const id = options.id;
+    const currentUserRef = db.collection('users').doc(currentUser.uid);
+    const tripRef = db.collection('trips').doc(id);
+    const activityRef = db.collection('activities').doc(id);
+
+    Object.keys(tripExpenses).forEach(expenseId => {
+    const expenseRef = db.collection('trips').doc(expenseId);
+        tripExpenses[expenseId].payees.forEach(payee => {
+            const userRef = db.collection('users').doc(payee.id);        
+            batch.update(userRef, {
+                expenses: firebase.firestore.FieldValue.arrayRemove(expenseRef),
+            });
+        });
+    });
+    batch.update(currentUserRef, {
+        trips: firebase.firestore.FieldValue.arrayRemove(tripRef),
+    });
+    batch.delete(tripRef);
+    batch.delete(activityRef);
+    
+    batch.commit();
+    console.log('leave trip done');
+}
+
+module.exports = functions.https.onCall(options => {
+    return leaveTrip(options);
+});
