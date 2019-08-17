23394d5943880923aaa5fb7e9e114967b55bee98
Kevin Lin <kevinlin505@hotmail.com>
2019-08-17 12:12:15 -0400

    remove unused

--- src/components/status-notification/StatusNotification.js
+++ src/components/status-notification/StatusNotification.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { CheckCircle, Close, Error, Info, Warning } from '@material-ui/icons';
