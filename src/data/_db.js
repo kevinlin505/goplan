66a5b7fb1786af736abe75d390993a7e97f99a99
Kevin <kevinlin505@users.noreply.github.com>
2019-08-27 18:27:50 -0400

    Include prod db and aws info (#72)
    * Include prod db and aws info
    
    * Include dev
    
    * firebase function environment setup' (#73)
    
    * firebase function environment setup

--- src/data/_db.js
+++ src/data/_db.js
@@ -3,7 +3,11 @@ import 'firebase/firestore';
 import Keys from '@constants/Keys';
 
 const initializeFireBase = () => {
-  firebase.initializeApp(Keys.FIREBASE);
+  if (process.env.production) {
+    firebase.initializeApp(Keys.FIREBASE);
+  } else {
+    firebase.initializeApp(Keys.FIREBASE_DEV);
+  }
 
   // State will be persisted even when the activity is destroyed.
   firebase.firestore().enablePersistence({ synchronizeTabs: true });
