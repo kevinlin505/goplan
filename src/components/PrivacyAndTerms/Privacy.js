12c6988e1041968d02ec0b098b08743d93eb30e3
kevinlin505 <kevinlin505@hotmail.com>
2019-08-27 00:01:38 -0400

    Update Privacy and Terms

---
+++ src/components/PrivacyAndTerms/Privacy.js
@@ -0,0 +1,219 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Privacy = () => {
+  return (
+    <Container>
+      <Link to="/home">Back</Link>
+      <Header>Privacy Policy</Header>
+      <SubTitle>Your privacy is critically important to us.</SubTitle>
+      GoPlan is located at:
+      <address>NY , United States</address>
+      <div>
+        Contact:{' '}
+        <a
+          href="mailto:goplanapp@gmail.com"
+          title="send an email to goplanapp@gmail.com"
+        >
+          goplanapp@gmail.com
+        </a>
+      </div>
+      <Copy>
+        It is GoPlan&#39;s policy to respect your privacy regarding any
+        information we may collect while operating our website. This Privacy
+        Policy applies to{' '}
+        <a href="https://goplantravelling.com">https://goplantravelling.com</a>{' '}
+        (hereinafter, &#34;us&#34;, &#34;we&#34;, or
+        &#34;https://goplantravelling.com&#34;). We respect your privacy and are
+        committed to protecting personally identifiable information you may
+        provide us through the Website. We have adopted this privacy policy
+        (&#34;Privacy Policy&#34;) to explain what information may be collected
+        on our Website, how we use this information, and under what
+        circumstances we may disclose the information to third parties. This
+        Privacy Policy applies only to information we collect through the
+        Website and does not apply to our collection of information from other
+        sources.
+      </Copy>
+      <Copy>
+        This Privacy Policy, together with the Terms and conditions posted on
+        our Website, set forth the general rules and policies governing your use
+        of our Website. Depending on your activities when visiting our Website,
+        you may be required to agree to additional terms and conditions.
+      </Copy>
+      <Title>Website Visitors</Title>
+      <Copy>
+        Like most website operators, GoPlan collects non-personally-identifying
+        information of the sort that web browsers and servers typically make
+        available, such as the browser type, language preference, referring
+        site, and the date and time of each visitor request. GoPlan&#39;s
+        purpose in collecting non-personally identifying information is to
+        better understand how GoPlan&#39;s visitors use its website. From time
+        to time, GoPlan may release non-personally-identifying information in
+        the aggregate, e.g., by publishing a report on trends in the usage of
+        its website.
+      </Copy>
+      <Copy>
+        GoPlan also collects potentially personally-identifying information like
+        Internet Protocol (IP) addresses for logged in users and for users
+        leaving comments on https://goplantravelling.com blog posts. GoPlan only
+        discloses logged in user and commenter IP addresses under the same
+        circumstances that it uses and discloses personally-identifying
+        information as described below.
+      </Copy>
+      <Title>Gathering of Personally-Identifying Information</Title>
+      <Copy>
+        Certain visitors to GoPlan&#39;s websites choose to interact with GoPlan
+        in ways that require GoPlan to gather personally-identifying
+        information. The amount and type of information that GoPlan gathers
+        depends on the nature of the interaction. For example, we ask visitors
+        who sign up for a blog at https://goplantravelling.com to provide a
+        username and email address.
+      </Copy>
+      <Title>Security</Title>
+      <Copy>
+        The security of your Personal Information is important to us, but
+        remember that no method of transmission over the Internet, or method of
+        electronic storage is 100% secure. While we strive to use commercially
+        acceptable means to protect your Personal Information, we cannot
+        guarantee its absolute security.
+      </Copy>
+      <Title>Advertisements</Title>
+      <Copy>
+        Ads appearing on our website may be delivered to users by advertising
+        partners, who may set cookies. These cookies allow the ad server to
+        recognize your computer each time they send you an online advertisement
+        to compile information about you or others who use your computer. This
+        information allows ad networks to, among other things, deliver targeted
+        advertisements that they believe will be of most interest to you. This
+        Privacy Policy covers the use of cookies by GoPlan and does not cover
+        the use of cookies by any advertisers.
+      </Copy>
+      <Title>Links To External Sites</Title>
+      <Copy>
+        Our Service may contain links to external sites that are not operated by
+        us. If you click on a third party link, you will be directed to that
+        third party&#39;s site. We strongly advise you to review the Privacy
+        Policy and terms and conditions of every site you visit.
+      </Copy>
+      <Copy>
+        We have no control over, and assume no responsibility for the content,
+        privacy policies or practices of any third party sites, products or
+        services.
+      </Copy>
+      <Title>
+        Https://goplantravelling.com uses Google AdWords for remarketing
+      </Title>
+      <Copy>
+        Https://goplantravelling.com uses the remarketing services to advertise
+        on third party websites (including Google) to previous visitors to our
+        site. It could mean that we advertise to previous visitors who
+        haven&#39;t completed a task on our site, for example using the contact
+        form to make an enquiry. This could be in the form of an advertisement
+        on the Google search results page, or a site in the Google Display
+        Network. Third-party vendors, including Google, use cookies to serve ads
+        based on someone&#39;s past visits. Of course, any data collected will
+        be used in accordance with our own privacy policy and Google&#39;s
+        privacy policy.
+      </Copy>
+      <Copy>
+        You can set preferences for how Google advertises to you using the
+        Google Ad Preferences page, and if you want to you can opt out of
+        interest-based advertising entirely by cookie settings or permanently
+        using a browser plugin.
+      </Copy>
+      <Title>Aggregated Statistics</Title>
+      <Copy>
+        GoPlan may collect statistics about the behavior of visitors to its
+        website. GoPlan may display this information publicly or provide it to
+        others. However, GoPlan does not disclose your personally-identifying
+        information.
+      </Copy>
+      <Title>Affiliate Disclosure</Title>
+      <Copy>
+        This site uses affiliate links and does earn a commission from certain
+        links. This does not affect your purchases or the price you may pay.
+      </Copy>
+      <Title>Cookies</Title>
+      <Copy>
+        To enrich and perfect your online experience, GoPlan uses
+        &#34;Cookies&#34;, similar technologies and services provided by others
+        to display personalized content, appropriate advertising and store your
+        preferences on your computer.
+      </Copy>
+      <Copy>
+        A cookie is a string of information that a website stores on a
+        visitor&#39;s computer, and that the visitor&#39;s browser provides to
+        the website each time the visitor returns. GoPlan uses cookies to help
+        GoPlan identify and track visitors, their usage of
+        https://goplantravelling.com, and their website access preferences.
+        GoPlan visitors who do not wish to have cookies placed on their
+        computers should set their browsers to refuse cookies before using
+        GoPlan&#39;s websites, with the drawback that certain features of
+        GoPlan&#39;s websites may not function properly without the aid of
+        cookies.
+      </Copy>
+      <Copy>
+        By continuing to navigate our website without changing your cookie
+        settings, you hereby acknowledge and agree to GoPlan&#39;s use of
+        cookies.
+      </Copy>
+      <Title>Privacy Policy Changes</Title>
+      <Copy>
+        Although most changes are likely to be minor, GoPlan may change its
+        Privacy Policy from time to time, and in GoPlan&#39;s sole discretion.
+        GoPlan encourages visitors to frequently check this page for any changes
+        to its Privacy Policy. Your continued use of this site after any change
+        in this Privacy Policy will constitute your acceptance of such change.
+      </Copy>
+      <Title></Title>
+      <Copy></Copy>
+      <Title>Credit & Contact Information</Title>
+      <Copy>
+        This privacy policy was created at{' '}
+        <a
+          href="https://termsandconditionstemplate.com/privacy-policy-generator/"
+          rel="noopener noreferrer"
+          target="_blank"
+          title="Privacy policy template generator"
+        >
+          termsandconditionstemplate.com
+        </a>
+        . If you have any questions about this Privacy Policy, please contact us
+        via <a href="mailto:goplanapp@gmail.com">email</a>
+      </Copy>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  max-width: ${({ theme }) => theme.sizes.large}px;
+  margin: 0 auto;
+  padding: 30px 24px;
+  background: ${({ theme }) => theme.colors.white};
+`;
+
+const Header = styled.div`
+  font-size: 28px;
+  font-weight: 700;
+  margin: 20px 0 10px;
+`;
+
+const Title = styled.div`
+  font-size: 24px;
+  font-weight: 600;
+  margin: 15px 0 10px;
+`;
+
+const SubTitle = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  margin: 5px 0;
+`;
+
+const Copy = styled.div`
+  margin: 10px 0;
+`;
+
+export default withRouter(Privacy);
