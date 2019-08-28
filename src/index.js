9dd876c9d05fb7e33f3c332aa8ca05d30cd9df79
yongbingao <ben.gao25@gmail.com>
2019-08-27 20:25:07 -0400

    Add prod enviornment check

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from 'react-dom';
 import App from '@components/App';
 
 // Global site tag (gtag.js) - Google Analytics
-const script = document.createElement('sript');
+if (process.env === 'production') {
+  const script = document.createElement('sript');
 
-script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-146416934-1';
-script.async = true;
-document.head.appendChild(script);
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-146416934-1';
+  script.async = true;
+  document.head.appendChild(script);
 
-window.dataLayer = window.dataLayer || [];
-function gtag(rest) {
-  window.dataLayer.push(rest);
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag(rest) {
+    window.dataLayer.push(rest);
+  }
+  gtag('js', new Date());
+  gtag('config', 'UA-146416934-1');
 }
-gtag('js', new Date());
-gtag('config', 'UA-146416934-1');
 
 ReactDOM.render(<App />, document.getElementById('root'));
